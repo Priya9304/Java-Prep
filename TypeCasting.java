f235ab4b113f86f000f9b1089ea6f7bcca80a50c
Priya9304 <pallavipriya9304@gmail.com>
2026-07-13 15:37:51 +0530

    Typecasting

---
+++ TypeCasting.java
@@ -0,0 +1,15 @@
+class TypeCasting{
+	public static void main(String[]args){
+		//datatype var = (datatype) var;
+		
+		double d = 120.78;
+		float f = (float)d;
+		long l = (long)f;
+		int i = (int)l;
+		
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(i);
+	}
+}
