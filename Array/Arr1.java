d04480626beb8de26ef50e3dc5533d274dcb42d1
Priya9304 <pallavipriya9304@gmail.com>
2026-07-18 15:22:19 +0530

    Array started

---
+++ Array/Arr1.java
@@ -0,0 +1,12 @@
+class Arr1{
+	public static void main(String[] args){
+		//Array
+		//dataType []var;
+		//var[] = {elm1,.........,elmn}--->CTE,because we declared previously and try to initiate later
+		
+		int a[] = {1,2,3};
+		for(int i=0; i<a.length;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
